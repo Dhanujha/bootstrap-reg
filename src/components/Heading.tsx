a6a45b72318ad319a61b5f31dd99fdcde0cbed5f
Dhanujha <rampradha2016@gmail.com>
2022-03-27 19:00:11 +0530

    [Dhanujha] Decreases the padding

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Heading({ heading }: { heading: string }) {
-  return <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">{heading}</h3>;
+  return <h3 className="mb-4 pb-1 pb-md-0 mb-md-3">{heading}</h3>;
 }
 
 export default Heading;
